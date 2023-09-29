8e1725dc6d4fc23e7e401467807ca547e987c8fd
Roland Leißa <leissa@uni-mannheim.de>
2023-09-29 22:44:19 +0200

    test ring

--- include/fe/ring.h
+++ include/fe/ring.h
@@ -11,11 +11,14 @@ namespace fe {
 template<class T, size_t N>
 class Ring {
 public:
-    void put(const T& t) {
+    Ring<T, N>& put(const T& t) {
         array_[first_] = t;
         first_ = (first_ + 1) % N;
+        return *this;
     }
 
+    T& front() { return (*this)[0]; }
+    const T& front() const { return (*this)[0]; }
     T& operator[](size_t i) { assert(i < N); return array_[(first_ + i) % N]; }
     const T& operator[](size_t i) const { assert(i < N); return array_[(first_ + i) % N]; }
 
@@ -28,7 +31,10 @@ private:
 template<class T>
 class Ring<T, 1> {
 public:
-    void push(const T& t) { t_ = t; }
+    void put(const T& t) { t_ = t; }
+
+    T& front() { return (*this)[0]; }
+    const T& front() const { return (*this)[0]; }
     T& operator[](size_t i) { assert_unused(i == 0); return t_; }
     const T& operator[](size_t i) const { assert_unused(i == 0); return t_; }
 
