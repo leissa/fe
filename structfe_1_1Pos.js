7a2943a4c12bd681045225d1215e27df01b14be4
leissa <leissa@users.noreply.github.com>
2023-09-30 21:02:24 +0000

    deploy: b30dd630af924c0a561cbc0678c337065c012546

--- structfe_1_1Pos.js
+++ structfe_1_1Pos.js
@@ -4,7 +4,7 @@ var structfe_1_1Pos =
     [ "Pos", "structfe_1_1Pos.html#a2f5a03e5db4071ac152db6f830c17732", null ],
     [ "Pos", "structfe_1_1Pos.html#a54c10d1c6f371ef318b340c1090a904a", null ],
     [ "operator bool", "structfe_1_1Pos.html#a6b95864d39d3bb4e00816d65ec6d052c", null ],
-    [ "operator==", "structfe_1_1Pos.html#af9e8857255b38f3e09b8a42e32e6698a", null ],
+    [ "operator<=>", "structfe_1_1Pos.html#a38026a3057acc9bb77de0e3951933c03", null ],
     [ "col", "structfe_1_1Pos.html#a1adde7ab9d0f30f4aeae62e76289da31", null ],
     [ "row", "structfe_1_1Pos.html#a989c45fc3d1ce613b90a8ab145c94158", null ]
 ];
