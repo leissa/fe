ff2d0052dec96b4761419ca95fd9798d3948228a
Roland Leißa <leissa@uni-mannheim.de>
2023-09-30 10:56:30 +0200

    ring: bug fixes + specialization for N=2

--- include/fe/ring.h
+++ include/fe/ring.h
@@ -11,14 +11,14 @@ namespace fe {
 template<class T, size_t N>
 class Ring {
 public:
-    Ring<T, N>& put(const T& t) {
+    Ring& put(const T& t) {
         array_[first_] = t;
         first_ = (first_ + 1) % N;
         return *this;
     }
 
-    T& front() { return (*this)[0]; }
-    const T& front() const { return (*this)[0]; }
+    T& front() { return array_[first_]; }
+    const T& front() const { return array_[first_]; }
     T& operator[](size_t i) { assert(i < N); return array_[(first_ + i) % N]; }
     const T& operator[](size_t i) const { assert(i < N); return array_[(first_ + i) % N]; }
 
@@ -31,15 +31,34 @@ private:
 template<class T>
 class Ring<T, 1> {
 public:
-    void put(const T& t) { t_ = t; }
+    Ring& put(const T& t) { t_ = t; return *this; }
 
-    T& front() { return (*this)[0]; }
-    const T& front() const { return (*this)[0]; }
+    T& front() { return t_; }
+    const T& front() const { return t_; }
     T& operator[](size_t i) { assert_unused(i == 0); return t_; }
     const T& operator[](size_t i) const { assert_unused(i == 0); return t_; }
 
 private:
     T t_;
+};
+
+/// Specialization if `N == 2`.
+template<class T>
+class Ring<T, 2> {
+public:
+    Ring& put(const T& t) {
+        array_[0] = array_[1];
+        array_[1] = t;
+        return *this;
+    }
+
+    T& front() { return array_[0]; }
+    const T& front() const { return array_[0]; }
+    T& operator[](size_t i) { assert_unused(i < 2); return array_[i]; }
+    const T& operator[](size_t i) const { assert_unused(i < 2); return array_[i]; }
+
+private:
+    std::array<T, 2> array_;
 };
 
 } // namespace fe
--- tests/test.cpp
+++ tests/test.cpp
@@ -5,29 +5,45 @@
 #include <doctest/doctest.h>
 
 TEST_CASE("ring1") {
-    fe::Ring<int, 1> ring;
-    ring.put(0);
-    CHECK(ring.front() == 0);
-    ring.put(1);
-    CHECK(ring.front() == 1);
-}
+    fe::Ring<int, 1> ring1;
+    ring1.put(0);
+    CHECK(ring1.front() == 0);
+    ring1.put(1);
+    CHECK(ring1.front() == 1);
 
-TEST_CASE("ring3") {
-    fe::Ring<int, 3> ring;
-    ring.put(0).put(1).put(2);
-    CHECK(ring[0] == 0);
-    CHECK(ring[1] == 1);
-    CHECK(ring[2] == 2);
-    ring.put(3);
-    CHECK(ring[0] == 1);
-    CHECK(ring[1] == 2);
-    CHECK(ring[2] == 3);
-    ring.put(4);
-    CHECK(ring[0] == 2);
-    CHECK(ring[1] == 3);
-    CHECK(ring[2] == 4);
-    ring.put(5);
-    CHECK(ring[0] == 3);
-    CHECK(ring[1] == 4);
-    CHECK(ring[2] == 5);
+    fe::Ring<int, 2> ring2;
+    ring2.put(0).put(1);
+    CHECK(ring2.front() == 0);
+    CHECK(ring2[0] == 0);
+    CHECK(ring2[1] == 1);
+    ring2.put(2);
+    CHECK(ring2.front() == 1);
+    CHECK(ring2[0] == 1);
+    CHECK(ring2[1] == 2);
+    ring2.put(3);
+    CHECK(ring2.front() == 2);
+    CHECK(ring2[0] == 2);
+    CHECK(ring2[1] == 3);
+
+    fe::Ring<int, 3> ring3;
+    ring3.put(0).put(1).put(2);
+    CHECK(ring3.front() == 0);
+    CHECK(ring3[0] == 0);
+    CHECK(ring3[1] == 1);
+    CHECK(ring3[2] == 2);
+    ring3.put(3);
+    CHECK(ring3.front() == 1);
+    CHECK(ring3[0] == 1);
+    CHECK(ring3[1] == 2);
+    CHECK(ring3[2] == 3);
+    ring3.put(4);
+    CHECK(ring3.front() == 2);
+    CHECK(ring3[0] == 2);
+    CHECK(ring3[1] == 3);
+    CHECK(ring3[2] == 4);
+    ring3.put(5);
+    CHECK(ring3.front() == 3);
+    CHECK(ring3[0] == 3);
+    CHECK(ring3[1] == 4);
+    CHECK(ring3[2] == 5);
 }
