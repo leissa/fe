1acd7b534023eccfbb1da1f32443937d4e38b3c9
Roland Leißa <leissa@uni-mannheim.de>
2024-04-03 03:33:21 +0200

    be a bit more straightforward dumping error ranges

--- include/fe/loc.cpp.h
+++ include/fe/loc.cpp.h
@@ -5,7 +5,7 @@ namespace fe {
 
 std::ostream& operator<<(std::ostream& os, const Pos pos) {
     if (pos.row) {
-        if (pos.col) return os << pos.row << " col " << pos.col;
+        if (pos.col) return os << pos.row << ':' << pos.col;
         return os << pos.row;
     }
     return os << "<unknown position>";
@@ -14,12 +14,7 @@ std::ostream& operator<<(std::ostream& os, const Pos pos) {
 std::ostream& operator<<(std::ostream& os, const Loc loc) {
     if (loc) {
         os << (loc.path ? loc.path->string() : "<unknown file>") << ':' << loc.begin;
-        if (loc.begin != loc.finis) {
-            if (loc.begin.row != loc.finis.row)
-                os << '-' << loc.finis;
-            else
-                os << '-' << loc.finis.col;
-        }
+        if (loc.begin != loc.finis) os << '-' << loc.finis;
         return os;
     }
     return os << "<unknown location>";
