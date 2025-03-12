8713b2c4236b36a9def927bdf4c885d3f0e7c1a0
Roland Leißa <leissa@uni-mannheim.de>
2025-03-12 15:15:41 +0100

    fixed uninitialized memory
    Also: more constexpr and noexcept

--- include/fe/sym.h
+++ include/fe/sym.h
@@ -42,15 +42,15 @@ public:
     static constexpr size_t Short_String_Mask  = Short_String_Bytes - 1;
 
     struct String {
-        String() noexcept = default;
-        String(size_t size)
+        constexpr String() noexcept = default;
+        constexpr String(size_t size) noexcept
             : size(size) {}
 
-        size_t size;
+        size_t size = 0;
         char chars[]; // This is actually a C-only features, but all C++ compilers support that anyway.
 
         struct Equal {
-            bool operator()(const String* s1, const String* s2) const {
+            constexpr bool operator()(const String* s1, const String* s2) const noexcept {
                 bool res = s1->size == s2->size;
                 for (size_t i = 0, e = s1->size; res && i != e; ++i) res &= s1->chars[i] == s2->chars[i];
                 return res;
@@ -58,13 +58,13 @@ public:
         };
 
         struct Hash {
-            size_t operator()(const String* s) const {
+            size_t operator()(const String* s) const noexcept {
                 return std::hash<std::string_view>()(std::string_view(s->chars, s->size));
             }
         };
 
 #ifdef FE_ABSL
-        template<class H> friend H AbslHashValue(H h, const String* string) {
+        template<class H> friend constexpr H AbslHashValue(H h, const String* string) noexcept {
             return H::combine(std::move(h), std::string_view(string->chars, string->size));
         }
 #endif
@@ -73,16 +73,16 @@ public:
     static_assert(sizeof(String) == sizeof(size_t), "String.chars should be 0");
 
 private:
-    Sym(uintptr_t ptr)
+    constexpr Sym(uintptr_t ptr) noexcept
         : ptr_(ptr) {}
 
 public:
-    Sym() noexcept = default;
+    constexpr Sym() noexcept = default;
 
     /// @name Getters
     ///@{
-    bool empty() const { return ptr_ == 0; }
-    size_t size() const {
+    constexpr bool empty() const noexcept { return ptr_ == 0; }
+    constexpr size_t size() const noexcept {
         if (empty()) return 0;
         if (auto size = ptr_ & Short_String_Mask) return size;
         return ((const String*)ptr_)->size;
@@ -91,48 +91,48 @@ public:
 
     /// @name Access
     ///@{
-    char operator[](size_t i) const {
+    constexpr char operator[](size_t i) const noexcept {
         assert(i < size());
         return c_str()[i];
     }
-    char front() const { return (*this)[0]; }
-    char back() const { return (*this)[size() - 1]; }
+    constexpr char front() const noexcept { return (*this)[0]; }
+    constexpr char back() const noexcept { return (*this)[size() - 1]; }
     ///@}
 
     /// @name Iterators
     ///@{
-    auto begin() const { return c_str(); }
-    auto end() const { return c_str() + size(); }
-    auto cbegin() const { return begin(); }
-    auto cend() const { return end(); }
-    auto rbegin() const { return std::reverse_iterator(end()); }
-    auto rend() const { return std::reverse_iterator(begin()); }
-    auto crbegin() const { return rbegin(); }
-    auto crend() const { return rend(); }
+    constexpr auto begin() const noexcept { return c_str(); }
+    constexpr auto end() const noexcept { return c_str() + size(); }
+    constexpr auto cbegin() const noexcept { return begin(); }
+    constexpr auto cend() const noexcept { return end(); }
+    constexpr auto rbegin() const noexcept { return std::reverse_iterator(end()); }
+    constexpr auto rend() const noexcept { return std::reverse_iterator(begin()); }
+    constexpr auto crbegin() const noexcept { return rbegin(); }
+    constexpr auto crend() const noexcept { return rend(); }
     ///@}
 
     /// @name Comparisons
     ///@{
-    auto operator<=>(Sym other) const { return this->view() <=> other.view(); }
-    bool operator==(Sym other) const { return this->ptr_ == other.ptr_; }
-    bool operator!=(Sym other) const { return this->ptr_ != other.ptr_; }
-    auto operator<=>(char c) const {
+    constexpr auto operator<=>(Sym other) const noexcept { return this->view() <=> other.view(); }
+    constexpr bool operator==(Sym other) const noexcept { return this->ptr_ == other.ptr_; }
+    constexpr bool operator!=(Sym other) const noexcept { return this->ptr_ != other.ptr_; }
+    constexpr auto operator<=>(char c) const noexcept {
         auto s = size();
         if (s == 0) return std::strong_ordering::less;
         auto cmp = (*this)[0] <=> c;
         if (s == 1) return cmp;
         return cmp == 0 ? std::strong_ordering::greater : cmp;
     }
-    auto operator==(char c) const { return (*this) <=> c == 0; }
-    auto operator!=(char c) const { return (*this) <=> c != 0; }
+    constexpr auto operator==(char c) const noexcept { return (*this) <=> c == 0; }
+    constexpr auto operator!=(char c) const noexcept { return (*this) <=> c != 0; }
     ///@}
 
     /// @name Conversions
     ///@{
-    const char* c_str() const { return view().data(); }
-    operator const char*() const { return c_str(); }
+    constexpr const char* c_str() const noexcept { return view().data(); }
+    constexpr operator const char*() const noexcept { return c_str(); }
 
-    std::string_view view() const {
+    constexpr std::string_view view() const noexcept {
         if (empty()) return {(const char*)&ptr_, 0};
         // Little endian: 2 a b 0 register: 0ba2
         // Big endian:    a b 0 2 register: ab02
@@ -140,19 +140,20 @@ public:
         if (auto size = ptr_ & Short_String_Mask) return {(const char*)&ptr_ + offset, size};
         return std::string_view(((const String*)ptr_)->chars, ((const String*)ptr_)->size);
     }
-    operator std::string_view() const { return view(); }
-    std::string_view operator*() const { return view(); }
+    constexpr operator std::string_view() const noexcept { return view(); }
+    constexpr std::string_view operator*() const noexcept { return view(); }
     // Unfortunately, this doesn't work:
     // std::string_view operator->() const { return view(); }
 
-    std::string str() const { return std::string(view()); } ///< This involves a copy.
-    explicit operator std::string() const { return str(); } ///< `explicit` as this involves a copy.
-
-    explicit operator bool() const { return ptr_; } ///< Is not empty?
+    constexpr std::string str() const noexcept { return std::string(view()); } ///< This involves a copy.
+    constexpr explicit operator std::string() const noexcept { return str(); } ///< `explicit` as this involves a copy.
+    constexpr explicit operator bool() const noexcept { return ptr_; }         ///< Is not empty?
     ///@}
 
 #ifdef FE_ABSL
-    template<class H> friend H AbslHashValue(H h, Sym sym) { return H::combine(std::move(h), sym.ptr_); }
+    template<class H> friend constexpr H AbslHashValue(H h, Sym sym) noexcept {
+        return H::combine(std::move(h), sym.ptr_);
+    }
 #endif
     friend struct ::std::hash<fe::Sym>;
     friend std::ostream& operator<<(std::ostream& o, Sym sym) { return o << sym.view(); }
@@ -169,7 +170,7 @@ private:
 } // namespace fe
 
 template<> struct std::hash<fe::Sym> {
-    size_t operator()(fe::Sym sym) const { return std::hash<uintptr_t>()(sym.ptr_); }
+    size_t operator()(fe::Sym sym) const noexcept { return std::hash<uintptr_t>()(sym.ptr_); }
 };
 
 namespace fe {
@@ -238,7 +239,9 @@ public:
     }
     Sym sym(const std::string& s) { return sym((std::string_view)s); }
     /// @p s is a null-terminated C-string.
-    Sym sym(const char* s) { return s == nullptr || *s == '\0' ? Sym() : sym(std::string_view(s, strlen(s))); }
+    constexpr Sym sym(const char* s) {
+        return s == nullptr || *s == '\0' ? Sym() : sym(std::string_view(s, strlen(s)));
+    }
     // TODO we can try to fit s in current page and hence eliminate the explicit use of strlen
     ///@}
 
