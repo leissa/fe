ba41eb86cefb2312b97eb534288f332610086bdf
Roland Leißa <roland.leissa@cs.uni-goettingen.de>
2026-05-09 09:43:51 +0200

    removed err(ln)/out(ln) in favor of std::print(ln)

--- include/fe/format.h
+++ include/fe/format.h
@@ -30,16 +30,6 @@ struct basic_ostream_formatter : std::formatter<std::basic_string_view<Char>, Ch
 
 using ostream_formatter = basic_ostream_formatter<char>;
 
-/// @name out/outln/err/errln
-/// Print to `std::cout`/`std::cerr` via `std::format`; the `*ln` variants conclude with `std::endl`.
-///@{
-// clang-format off
-template<class... Args> void err  (std::format_string<Args...> fmt, Args&&... args) { std::cerr << std::format(fmt, std::forward<Args>(args)...);              }
-template<class... Args> void out  (std::format_string<Args...> fmt, Args&&... args) { std::cout << std::format(fmt, std::forward<Args>(args)...);              }
-template<class... Args> void errln(std::format_string<Args...> fmt, Args&&... args) { std::cerr << std::format(fmt, std::forward<Args>(args)...) << std::endl; }
-template<class... Args> void outln(std::format_string<Args...> fmt, Args&&... args) { std::cout << std::format(fmt, std::forward<Args>(args)...) << std::endl; }
-// clang-format on
-
 /// Keeps track of indentation level during output
 class Tab {
 public:
